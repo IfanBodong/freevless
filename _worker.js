83607f7cfd62d2c4f34cfabc79b60386d90e779d
emilwhatsapp <117849900+emilwhatsapp@users.noreply.github.com>
2024-01-28 15:47:04 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -6,7 +6,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = '54e0c0a4-3ef4-4071-bac0-47ae9386ff2c';
 
-const proxyIPs = ['104.28.213.124'];
+const proxyIPs = ['192.53.173.162'];
 
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
 
